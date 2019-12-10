cfeb47d4e12a730d47e2a86804247f5fff7f524d
YoonGyeong <45262992+Yg323@users.noreply.github.com>
2019-12-11 02:37:24 +0900

    e
    e

--- korea.js
+++ korea.js
@@ -5,6 +5,14 @@ $(document).ready(function () {
 	// select 선택이 변경 됐을 때 호출 되는 함수
 		$('.select_travel').change(function() {
 		$('.puzzle_travel').text($(".select_travel option:selected").val());
+		$( ".puzzle_travel" ).draggable({
+			helper:"clone",
+			cursor:"move",
+			start: function(event,ui){
+				$(".day_trip").css("backgroud-color","red");
+			},
+			distance: 10
+		});
 	});
 	// 초기 값 지정
 	$('.puzzle_food').text("추천 코스를 편집해주세요!");
@@ -15,13 +23,23 @@ $(document).ready(function () {
 });
 // 화면 전환
 function j_test(n){
-	$('.css_test').scrollLeft( $('.css_test').scrollLeft() + n );
+	$('.day_change').scrollLeft( $('.day_change').scrollLeft() + n );
 }
 // 드래그앤 드롭
-$( function() {
-	$( ".puzzle_travel" ).draggable();
-	$( '.puzzle_food' ).draggable();
-  } );
+// $( function() {
+// 	$( ".puzzle_travel" ).draggable({
+// 		helper:"clone"
+// 	});
+// 	$( '.puzzle_food' ).draggable();
+
+	/* $("").droppable({
+		drop: function(event, ui){
+			$(this)
+			.addclass("")
+		}
+	}); */
+
+//   } );
 
 
 
