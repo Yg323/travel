b40cbbdb5d7d4b71ccfa9b94bf564de6a5b72a33
YoonGyeong <45262992+Yg323@users.noreply.github.com>
2019-12-11 03:53:38 +0900

    df

--- korea.js
+++ korea.js
@@ -20,17 +20,25 @@ $(document).ready(function () {
 		$('.select_food').change(function() {
 		$('.puzzle_food').text($(".select_food option:selected").val());
 	});
+	$('.puzzle_map').text("추천 코스를 편집해주세요!");
+	// select 선택이 변경 됐을 때 호출 되는 함수
+		$('.select_map').change(function() {
+		$('.puzzle_map').text($(".select_map option:selected").val());
+	});
 });
 // 화면 전환
 function j_test(n){
 	$('.day_change').scrollLeft( $('.day_change').scrollLeft() + n );
 }
-// 드래그앤 드롭
-// $( function() {
-// 	$( ".puzzle_travel" ).draggable({
-// 		helper:"clone"
-// 	});
-// 	$( '.puzzle_food' ).draggable();
+//드래그앤 드롭
+$( function() {
+	$( ".select_trans" ).draggable({
+		helper:"clone"
+		
+	});
+	$(".day_trip").droppable({
+		accept: ".select_trans"
+	});
 
 	/* $("").droppable({
 		drop: function(event, ui){
@@ -39,7 +47,7 @@ function j_test(n){
 		}
 	}); */
 
-//   } );
+} );
 
 
 
