4d0f397029241064ebbaed63eda21eb904d5bfcf
YoonGyeong <45262992+Yg323@users.noreply.github.com>
2019-12-12 01:36:38 +0900

    ㅇㄹ

--- korea.js
+++ korea.js
@@ -9,57 +9,73 @@ $(document).ready(function () {
 			helper:"clone",
 			cursor:"move",
 			start: function(event,ui){
-				$(".day_trip").css("backgroud-color","red");
+				$(".day_text1").css("border","dashed 0.1px red");
 			},
 			distance: 10
 		});
-		var mapContainer = document.getElementById('right3'), // 지도를 표시할 div 
-    	mapOption = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
-        level: 3 // 지도의 확대 레벨
-    	};  
-
-		// 지도를 생성합니다    
-		var map = new kakao.maps.Map(mapContainer, mapOption); 
-
-		// 주소-좌표 변환 객체를 생성합니다
-		var geocoder = new kakao.maps.services.Geocoder();
-
-		// 주소로 좌표를 검색합니다
-		geocoder.addressSearch($(".select_travel option:selected"), function(result, status) {
-
-			// 정상적으로 검색이 완료됐으면 
-			if (status === kakao.maps.services.Status.OK) {
-
-				var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
-
-				// 결과값으로 받은 위치를 마커로 표시합니다
-				var marker = new kakao.maps.Marker({
-					map: map,
-					position: coords
-				});
-
-				// 인포윈도우로 장소에 대한 설명을 표시합니다
-				var infowindow = new kakao.maps.InfoWindow({
-					content: '<div style="width:150px;text-align:center;padding:6px 0;">요기요기</div>'
-				});
-				infowindow.open(map, marker);
-
-				// 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
-				map.setCenter(coords);
-			} 
-		});    
+		$(".day_container1").droppable({
+			accept: ".puzzle_travel",
+			classes : {
+				"ui-droppable-active": "ui-state-active",
+				"ui-droppable-hover": "ui-state-hover"
+			  },
+			drop: function(event,ui){
+				$(".day_text1").css("border","0px");
+				$('.day_trip img',this).attr('src','img/여행지 아이콘.png');
+				$('.day_text1',this).text($(".select_travel option:selected").val());
+			}
+		});
 	});
 	// 초기 값 지정
 	$('.puzzle_food').text("추천 코스를 편집해주세요!");
 	// select 선택이 변경 됐을 때 호출 되는 함수
 		$('.select_food').change(function() {
 		$('.puzzle_food').text($(".select_food option:selected").val());
+		$( ".puzzle_food" ).draggable({
+			helper:"clone",
+			cursor:"move",
+			start: function(event,ui){
+				$(".day_text1").css("border","dashed 0.1px red");
+			},
+			distance: 10
+		});
+		$(".day_container1").droppable({
+			accept: ".puzzle_food",
+			classes : {
+				"ui-droppable-active": "ui-state-active",
+				"ui-droppable-hover": "ui-state-hover"
+			  },
+			drop: function(event,ui){
+				$(".day_text1").css("border","0px");
+				$('.day_trip img',this).attr('src','img/음식 아이콘.png');
+				$('.day_text1',this).text($(".select_food option:selected").val());
+			}
+		});
 	});
 	$('.puzzle_map').text("추천 코스를 편집해주세요!");
 	// select 선택이 변경 됐을 때 호출 되는 함수
 		$('.select_map').change(function() {
 		$('.puzzle_map').text($(".select_map option:selected").val());
+		$( ".puzzle_map" ).draggable({
+			helper:"clone",
+			cursor:"move",
+			start: function(event,ui){
+				$(".day_text1").css("border","dashed 0.1px red");
+			},
+			distance: 10
+		});
+		$(".day_container1").droppable({
+			accept: ".puzzle_map",
+			classes : {
+				"ui-droppable-active": "ui-state-active",
+				"ui-droppable-hover": "ui-state-hover"
+			  },
+			drop: function(event,ui){
+				$(".day_text1").css("border","0px");
+				$('.day_trip img',this).attr('src','img/집주소 아이콘.png');
+				$('.day_text1',this).text($(".select_map option:selected").val());
+			}
+		});
 	});
 });
 // 화면 전환
@@ -68,20 +84,97 @@ function j_test(n){
 }
 //드래그앤 드롭
 $( function() {
-	$( ".select_trans" ).draggable({
-		helper:"clone"
-		
+	$( ".select_trans_w img" ).draggable({
+		helper:"clone",
+		cursor:"move",
+		start: function(event,ui){
+			$(".day_text").css("border","dashed 0.1px red");
+		},
+		distance: 10
 	});
-	$(".day_trip").droppable({
-		accept: ".select_trans"
-	});
-
-	/* $("").droppable({
-		drop: function(event, ui){
-			$(this)
-			.addclass("")
+	$(".day_container").droppable({
+		accept: ".select_trans_w img",
+		classes : {
+			"ui-droppable-active": "ui-state-active",
+			"ui-droppable-hover": "ui-state-hover"
+		  },
+		drop: function(event,ui){
+			$(".day_text").css("border","0px");
+			$('.day_trans img',this).attr('src','img/걷는 아이콘.png');
+			$(".day_trans img",this).css("width","auto");
+			$(".day_trans img",this).css("height","50px");
+			// $(".day_trans img",this).css("padding-top","25px");
 		}
-	}); */
+	});
+});
+$( function() {
+	$( ".select_trans_b img" ).draggable({
+		helper:"clone",
+		cursor:"move",
+		start: function(event,ui){
+			$(".day_text").css("border","dashed 0.1px red");
+		},
+		distance: 10
+	});
+	$(".day_container").droppable({
+		accept: ".select_trans_b img",
+		classes : {
+			"ui-droppable-active": "ui-state-active",
+			"ui-droppable-hover": "ui-state-hover"
+		  },
+		drop: function(event,ui){
+			$(".day_text").css("border","0px");
+			$('.day_trans img',this).attr('src','img/버스 아이콘.png');
+		}
+	});
+});
+$( function() {
+	$( ".select_trans_c img" ).draggable({
+		helper:"clone",
+		cursor:"move",
+		start: function(event,ui){
+			$(".day_text").css("border","dashed 0.1px red");
+		},
+		distance: 10
+	});
+	$(".day_container").droppable({
+		accept: ".select_trans_c img",
+		classes : {
+			"ui-droppable-active": "ui-state-active",
+			"ui-droppable-hover": "ui-state-hover"
+		  },
+		drop: function(event,ui){
+			$(".day_text").css("border","0px");
+			$('.day_trans img',this).attr('src','img/차 아이콘.png');
+			$(".day_trans img",this).css("width","64px");
+			$(".day_trans img",this).css("height","25px");
+			$(".day_trans img",this).css("padding-top","25px");
+		}
+	});
+});
+$( function() {
+	$( ".select_trans_t img" ).draggable({
+		helper:"clone",
+		cursor:"move",
+		start: function(event,ui){
+			$(".day_text").css("border","dashed 0.1px red");
+		},
+		distance: 10
+	});
+	$(".day_container").droppable({
+		accept: ".select_trans_t img",
+		classes : {
+			"ui-droppable-active": "ui-state-active",
+			"ui-droppable-hover": "ui-state-hover"
+		  },
+		drop: function(event,ui){
+			$(".day_text").css("border","0px");
+			$('.day_trans img',this).attr('src','img/기차 아이콘.png');
+			$(".day_trans img",this).css("width","64px");
+			$(".day_trans img",this).css("height","25px");
+			$(".day_trans img",this).css("padding-top","25px");
+		}
+	});
 
 } );
 
